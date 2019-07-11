6b6eda7ca520957b9548372d9ddf03489cc7f5c6
uniquebum <miroruopsa@gmail.com>
2019-07-11 17:22:37 +0300

    testin...

--- headers/GlfwHeaders.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <GLFW/glfw3.h>
-#include "./EngineConstants.h"
-
-#include "./GlfwSquare.h"
-#include "./GlfwGameControl.h"
--- headers/engineConstants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-static const int W_WIDTH = 1280;
-static const int W_HEIGHT = 800;
-static const float M_PI = 3.1415926;
