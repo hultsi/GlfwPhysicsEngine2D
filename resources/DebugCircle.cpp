e06e0d21e63ce078364faca13ff188fab62736b1
Miro R <38552014+uniquebum@users.noreply.github.com>
2019-08-15 17:22:09 +0300

    Create DebugCircle.cpp

---
+++ resources/DebugCircle.cpp
@@ -0,0 +1,23 @@
+#include "./../headers/GlfwHeaders.h"
+#include <cmath>
+#include <iostream>
+
+DebugCircle::DebugCircle(float x, float y, float radius) : x(x),y(y),radius(radius){
+    float theta = 2*M_PI/16;
+    for (int i = 0; i < 32; i += 4) {
+        lineVertices[i] = x + radius*std::cos((i/2)*theta);
+        lineVertices[i+1] = y + radius*std::sin((i/2)*theta);
+
+        lineVertices[i+2] = x + radius*std::cos((i/2 + 2)*theta);
+        lineVertices[i+3] = y + radius*std::sin((i/2 + 2)*theta);
+    }
+}
+
+
+void DebugCircle::draw() {
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, lineVertices);
+    glDrawArrays(GL_LINES, 0, 16);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
